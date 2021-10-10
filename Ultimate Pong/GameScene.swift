09e9949ae8a45a8bed7696df908229df538a4a05
Andrew <armast97@gmail.com>
2021-10-09 20:30:47 -0700

    fixing game physics

--- Ultimate Pong/GameScene.swift
+++ Ultimate Pong/GameScene.swift
@@ -30,6 +30,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
 //        addChild(background)
 //        backgroundLights?.position = CGPoint(x: size.width / 2, y: size.height / 2)
 //        addChild(backgroundLights!)
+        self.scene?.physicsWorld.contactDelegate = self
         
         border = SKPhysicsBody(edgeLoopFrom: self.frame)
         border.friction = 0
@@ -37,6 +38,9 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         self.physicsBody = border
         
         ball = self.childNode(withName: "ball") as! SKSpriteNode
+//        ball.physicsBody?.restitution = 1
+//        ball.physicsBody?.usesPreciseCollisionDetection = true
+        
         rightPaddle = self.childNode(withName: "rightPaddle") as! SKSpriteNode
         leftPaddle = self.childNode(withName: "leftPaddle") as! SKSpriteNode
         leftGoal = self.childNode(withName: "leftGoal") as! SKSpriteNode
@@ -45,8 +49,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         scoreRightLbl = self.childNode(withName: "scoreRight") as! SKLabelNode
         
         ball.physicsBody?.collisionBitMask = 0b0001
-        leftGoal.physicsBody?.collisionBitMask = 0b0010
-
         rightPaddle.physicsBody?.categoryBitMask = 0b0001
         leftPaddle.physicsBody?.categoryBitMask = 0b0001
         leftGoal.physicsBody?.categoryBitMask = 0b0001
@@ -70,9 +72,9 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     override func update(_ currentTime: TimeInterval) {
-        
         leftPaddle.run(SKAction.moveTo(y: ball.position.y, duration: 0.1))
     }
+    
     
     func didBegin(_ contact: SKPhysicsContact) {
         var ball = contact.bodyA
@@ -81,19 +83,28 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             ball = contact.bodyB
             object = contact.bodyA
         }
-        if(object.collisionBitMask == 0b0010){
-            let goal = object.node as? SKSpriteNode
-            if goal == self.leftGoal {
-                print("Adding to Right Goal")
-                scoreRightNum = scoreRightNum + 1
-                scoreRightLbl.text = "\(scoreRightNum)"
-            } else if goal == self.rightGoal {
-                print("Adding to Left Goal")
-                scoreLeftNum = scoreLeftNum + 1
-                scoreLeftLbl.text = "\(scoreLeftNum)"
-            }
+        if ball.node?.name == "ball" {
+            print("BALL IS INDEED, BALL")
         }
-        print("Adding Speed")
-        ball.node?.physicsBody?.applyImpulse(CGVector(dx: 5, dy: 5))
+        if object.node?.name == "leftPaddle" {
+            print("HIT LEFT PADDLE")
+            ball.node?.physicsBody?.applyImpulse(CGVector(dx: 5, dy: -2))
+        }
+        else if object.node?.name == "rightPaddle" {
+            print("HIT RIGHT PADDLE")
+            ball.node?.physicsBody?.applyImpulse(CGVector(dx: -5, dy: 2))
+        }
+        else if object.node?.name == "leftGoal" {
+            print("HIT LEFT GOAL")
+            scoreRightNum += 1
+            scoreRightLbl.text = "\(scoreRightNum)"
+            ball.node?.physicsBody?.applyImpulse(CGVector(dx: -5, dy: 2))
+        }
+        else if object.node?.name == "rightGoal" {
+            print("HIT RIGHT GOAL")
+            scoreLeftNum += 1
+            scoreLeftLbl.text = "\(scoreLeftNum)"
+            ball.node?.physicsBody?.applyImpulse(CGVector(dx: -5, dy: -2))
+        }
     }
 }
